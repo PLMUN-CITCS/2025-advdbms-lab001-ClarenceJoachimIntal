69cc8da3ddc8cc62635871e77ce7ca829675e505
Clarence Joachim Intal <intalclarencejoachim_bsit@plmun.edu.ph>
2025-03-19 23:24:34 +0800

    Submitting ADVDBMS SQL Lab Project Database Assignment

---
+++ SQL_Lab_Project/scripts/01_create_tables.sql
@@ -0,0 +1,8 @@
+-- Creating tblStudents table
+CREATE TABLE tblStudents (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_name VARCHAR(50) NOT NULL,
+    student_email VARCHAR(50) UNIQUE NOT NULL,
+    student_phone VARCHAR(20) NOT NULL
+);
+
